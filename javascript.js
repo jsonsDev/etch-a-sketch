af1b1a619f69e4fca419f1602838542170211448
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-08-09 19:31:41 +0800

    Add event listener to handle colored squares on mouseover.

--- javascript.js
+++ javascript.js
@@ -6,6 +6,9 @@ for (let i = 0; i <256; i++) {
 const gridSquare = document.createElement("div");
 gridSquare.classList.add("grid-square");
 container.appendChild(gridSquare);
+gridSquare.addEventListener("mouseover", () => {
+    gridSquare.classList.add("etched-grid");
+});
 };
 
 const buttonContainer = document.querySelector(".button-container");
@@ -13,3 +16,8 @@ const requestButton = document.createElement("button");
 requestButton.classList.add("request-button");
 requestButton.textContent = "Create new grid";
 buttonContainer.appendChild(requestButton);
+
+const createGridButton = document.querySelector(".request-button");
+createGridButton.addEventListener("click", () => {
+    prompt("Enter number of squares per side to create a new grid");
+});
