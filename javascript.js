85ff7c8cc7073299001907d592f53feb2bc0b6cd
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-08-14 13:34:54 +0800

    Add logic to remove existing grid and replace with new grid from user input.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,8 @@
-// Create a 16x16 grid
-
 const container = document.querySelector(".grid-container");
 
-for (let i = 0; i <256; i++) {
-const gridSquare = document.createElement("div");
-gridSquare.classList.add("grid-square");
-container.appendChild(gridSquare);
-gridSquare.addEventListener("mouseover", () => {
-    gridSquare.classList.add("etched-grid");
-});
-};
+let gridSize = 16;
+
+createGrid();
 
 const buttonContainer = document.querySelector(".button-container");
 const requestButton = document.createElement("button");
@@ -19,5 +12,38 @@ buttonContainer.appendChild(requestButton);
 
 const createGridButton = document.querySelector(".request-button");
 createGridButton.addEventListener("click", () => {
-    prompt("Enter number of squares per side to create a new grid");
+    removeGrid();
+    let input = prompt("Enter number of squares per side to create a new grid");
+    gridSize = parseInt(input);
+    
+    if (isNaN(gridSize)) {
+        input = prompt("Not a number! Try again!");
+    } else if(gridSize > 100) {
+        input = prompt("Keep it under 100:");
+    } else {
+        input = gridSize;
+        createGrid();
+    }
 });
+
+function removeGrid() {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridRemove = document.querySelector(".grid-square");
+    gridRemove.remove();
+    };
+};
+
+
+function createGrid() {
+    let = squareSize = container.clientWidth / gridSize;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    gridSquare.style.width = `${squareSize}px`;
+    gridSquare.style.height = `${squareSize}px`;
+    container.appendChild(gridSquare);
+    gridSquare.addEventListener("mouseover", () => {
+        gridSquare.classList.add("etched-grid");
+    });
+    };
+};
